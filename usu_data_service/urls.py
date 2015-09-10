2b5d8f1c352b1d9346705f7a96ce968494f1d726
pkdash <pkdash_reena@hotmail.com>
2015-09-10 14:26:08 -0600

    added root url to display capabilities list

--- usu_data_service/urls.py
+++ usu_data_service/urls.py
@@ -4,6 +4,7 @@ from ciwater import settings
 from usu_data_service import views
  
 urlpatterns = patterns('',
+  url(r'^$', views.show_capabilities),
   url(r'^api/dataservice/(?P<func>[A-z0-9]+)$', views.RunService.as_view()),
   url(r'^api/dataservice/showstaticdata/info', views.show_static_data_info),
   url(r'^api/dataservice/myfiles/list', views.show_my_files),
