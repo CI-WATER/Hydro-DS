dde4f4303fa2faf846304578af2078e80c58eb09
gantian127 <jamy127@foxmail.com>
2017-03-20 18:47:51 -0600

    job submit: add run_job.py

---
+++ usu_data_service/servicefunctions/run_job.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
+
+
+def run_service(func, **kwargs):
+    pool = ThreadPoolExecutor(1)
+    future = pool.submit(func, **kwargs)
+    pool.shutdown(wait=False)
+
+    return future
+
+
+def run_service_done(future, job):
+    job.end_time = datetime.now()
+    job.status = 'Done'
+    if future.exception():
+        job.is_success = False
+        job.message = 'The job {} is failed: {}'.format(job.job_description, future.exception())
+    elif future.cancelled():
+        job.is_success = False
+        job.message = 'The job {} is cancelled.'.format(job.job_description)
+    elif future.result():
+        result = future.result()
+        if result['success'] == 'True':
+            job.is_success = True
+            job.message = result['message']
+        else:
+            job.is_success = False
+            job.message = 'The job {} is failed: {}'.format(job.job_description, result['message'])
+
+    job.save()
