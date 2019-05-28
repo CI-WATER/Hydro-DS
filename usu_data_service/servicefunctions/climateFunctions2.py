9d3b58fb4ab25c074091f42dc075d8f792be3f18
tseganeh <zacctsega@yahoo.com>
2019-05-28 14:22:34 -0600

    forcing downscaling
    Codes for downscaling Precipitation, Temperature, Vapor pressure updated

--- usu_data_service/servicefunctions/climateFunctions2.py
+++ usu_data_service/servicefunctions/climateFunctions2.py
@@ -66,6 +66,38 @@ def get_raster_Difference(input_raster, reference_raster, output_raster):
     # print()
     return retDictionary
 
+def adjust_for_elevation_Forcing(input_netcdf, output_netcdf, varName, input_raster, target_raster,
+                                 time_var_name='time', baseDateTime='2008/10/01 0', timeUnits='hours', varCode= 'temp'):
+    """
+    adjusts for elevation based on specified var name
+    :param input_netcdf: input
+    :param output_netcdf: output
+    :param varName: name of variable of interest
+    :param input_raster: raster containing the DEM values used when generating the original climate data (in input_netcdf)
+    :param target_raster: temperature values adjusted to this DEM
+    :return:
+    """
+    if(varCode == 'temp'):
+        retDictionary = adjust_for_elevation_Temperature(input_netcdf=input_netcdf, output_netcdf=output_netcdf, varName=varName,
+                                         input_raster=input_raster, target_raster=target_raster, time_var_name=time_var_name,
+                                         baseDateTime=baseDateTime, timeUnits=timeUnits)
+    elif(varCode == 'prec'):
+            retDictionary = adjust_for_elevation_Precipitation(input_netcdf=input_netcdf, output_netcdf=output_netcdf, varName=varName,
+                                             input_raster=input_raster, target_raster=target_raster,
+                                             time_var_name=time_var_name,
+                                             baseDateTime=baseDateTime, timeUnits=timeUnits)
+    elif(varCode == 'vp'):
+            retDictionary = adjust_for_elevation_VaporPressure(input_netcdf=input_netcdf, output_netcdf=output_netcdf, varName=varName,
+                                             input_raster=input_raster, target_raster=target_raster,
+                                             time_var_name=time_var_name,
+                                             baseDateTime=baseDateTime, timeUnits=timeUnits)
+    else:
+        logger.info("Error elevation adjustment; wrong variable specier; must be 'temp', 'prec', or 'vp' ")
+        retDictionary = {'success': "False", 'message': " Error elevation adjustment; wrong variable specier; must be 'temp', 'prec', or 'vp' "}
+
+
+    return retDictionary
+
 
 def adjust_for_elevation_Temperature(input_netcdf, output_netcdf, varName, input_raster, target_raster, time_var_name='time', baseDateTime='1980/01/01 0', timeUnits='days'):
     """
@@ -210,82 +242,6 @@ def adjust_for_elevation_Precipitation(input_netcdf, output_netcdf, varName, inp
     return retDictionary
 
 
-#This uses P = P0*((T0 + lambda*dz)/T0)^(-g/lambda*R)
-def adjust_for_elevation_SurfaceAirPressure(input_netcdfP,  varNameP, input_netcdfT,  varNameT, output_netcdf, input_raster, target_raster, time_var_name='time', baseDateTime='1980/01/01 0', timeUnits='days'):
-    """
-    ajusts temerature values with lapse rate based on elevaton
-    :param input_netcdf: input
-    :param output_netcdf: output
-    :param varName: name of variable of interest
-    :param input_raster: raster containing the DEM values used when generating the original climate data (in input_netcdf)
-    :param target_raster: temperature values adjusted to this DEM
-    :return:
-    """
-    # lapse rates, monthly   ToDo: check values
-    lapserateT = [0.0044,0.0059,0.0071,0.0078,0.0081,0.0082,0.0081,0.0081,0.0077,0.0068,0.0055,0.0047]
-    grav = 9.81        # m/s^2
-    Rgas = 287.04      # J/kg-k
-
-    #copy variables and attriutes to output netcdf
-    cmdString = "nccopy -k 3 "+input_netcdfP+" "+output_netcdf
-    callSubprocess(cmdString, 'copy netcdf')
-
-    #create elevation difference dem raster
-    dzDEM = os.path.join(os.path.dirname(output_netcdf),'dzDEM_temp.tif')
-    #cmdString = "python gdal_calc.py -A " + input_raster + " -B " + target_raster + " --outfile=" + dzDEM + " --calc=B-A"
-    #retDictionary = call_subprocess(cmdString, "compute elevation difference")\
-    retDictionary = get_raster_Difference(input_raster, target_raster, dzDEM)
-    if retDictionary['success'] == "False":
-        return retDictionary
-
-    #get value of dzDEM array
-    ref_data = gdal.Open(dzDEM, GA_ReadOnly)
-    inband = ref_data.GetRasterBand(1)
-    #nodata = inband.GetNoDataValue()
-    dzDEMArray = inband.ReadAsArray()
-    dzDEMArrayout = dzDEMArray[::-1]         # 8.12.15 gdal and netcdf4 have arrays in opposite directions
-    #dType = inband.DataType
-    ref_data = None
-
-    #Open file to write re-gridded data
-    ncOut = netCDF4.Dataset(output_netcdf,"r+") #, format='NETCDF4')
-    xout = ncOut.variables['x'][:]
-    yout = ncOut.variables['y'][:]
-    vardataType = ncOut.variables[varNameP].datatype
-    varin = numpy.zeros((len(yout),len(xout)),dtype=vardataType)
-    varout = numpy.zeros((len(yout),len(xout)),dtype=vardataType)
-    timeLen = len(ncOut.dimensions['time'])
-    tArray = ncOut.variables[time_var_name][:]
-    yLen = len(yout)
-    xLen = len(xout)
-
-    ncInT = netCDF4.Dataset(input_netcdfT,"r") # format='NETCDF4')
-    for tk in range(timeLen):
-        varin[:,:] = ncOut.variables[varNameP][tk,:,:]
-        varinT = ncInT.variables[varNameT][tk,:,:]
-        if timeUnits == 'days':
-            dateTimetk = datetime.strptime(baseDateTime,"%Y/%m/%d %H") + timedelta(days=int(tArray[tk]))
-        elif timeUnits == 'hours':
-            dateTimetk = datetime.strptime(baseDateTime,"%Y/%m/%d %H") + timedelta(hours=int(tArray[tk]))
-        else:
-            logger.info("Erroneous time unit")
-            retDictionary = {'success': "False", 'message': "Erroneous time unit"}
-            #print()
-            return retDictionary
-        monthtk = int(dateTimetk.month - 1)
-        #for yi in range(yLen):
-        #    for xj in range(xLen):
-        #       varout[yi,xj] = varin[yi,xj] - (dzDEMArrayout[yi,xj] * lapserateT[monthtk])  # bilinear_interpolation(yout[yi],xout[xj],points)
-        varout = varin* math.pow((((varinT+273.16) + dzDEMArrayout*-1*lapserateT[monthtk])/(varinT+273.16)),(-1*grav/(Rgas*-1*lapserateT[monthtk])))     #T in oC and lapserrate +ve
-        ncOut.variables[varNameP][tk,:,:] = varout[:,:]
-
-    ncOut.close()
-
-    logger.info("Success elevation adjustment Surface pressure")
-    retDictionary = {'success': "True", 'message': " Done surface pressure elevation adjustment"}
-    return retDictionary
-
-
 def adjust_for_elevation_VaporPressure(input_netcdf, output_netcdf, varName, input_raster, target_raster, time_var_name='time', baseDateTime='2008/10/01 0', timeUnits='hours'):
     """
     adjusts values with lapse rate based on elevaton
@@ -367,5 +323,79 @@ def adjust_for_elevation_VaporPressure(input_netcdf, output_netcdf, varName, inp
     return retDictionary
 
 
+#This uses P = P0*((T0 + lambda*dz)/T0)^(-g/lambda*R)
+def adjust_for_elevation_SurfaceAirPressure(input_netcdfP,  varNameP, input_netcdfT,  varNameT, output_netcdf, input_raster, target_raster, time_var_name='time', baseDateTime='1980/01/01 0', timeUnits='days'):
+    """
+    ajusts temerature values with lapse rate based on elevaton
+    :param input_netcdf: input
+    :param output_netcdf: output
+    :param varName: name of variable of interest
+    :param input_raster: raster containing the DEM values used when generating the original climate data (in input_netcdf)
+    :param target_raster: temperature values adjusted to this DEM
+    :return:
+    """
+    # lapse rates, monthly   ToDo: check values
+    lapserateT = [0.0044,0.0059,0.0071,0.0078,0.0081,0.0082,0.0081,0.0081,0.0077,0.0068,0.0055,0.0047]
+    grav = 9.81        # m/s^2
+    Rgas = 287.04      # J/kg-k
+
+    #copy variables and attriutes to output netcdf
+    cmdString = "nccopy -k 3 "+input_netcdfP+" "+output_netcdf
+    callSubprocess(cmdString, 'copy netcdf')
+
+    #create elevation difference dem raster
+    dzDEM = os.path.join(os.path.dirname(output_netcdf),'dzDEM_temp.tif')
+    #cmdString = "python gdal_calc.py -A " + input_raster + " -B " + target_raster + " --outfile=" + dzDEM + " --calc=B-A"
+    #retDictionary = call_subprocess(cmdString, "compute elevation difference")\
+    retDictionary = get_raster_Difference(input_raster, target_raster, dzDEM)
+    if retDictionary['success'] == "False":
+        return retDictionary
+
+    #get value of dzDEM array
+    ref_data = gdal.Open(dzDEM, GA_ReadOnly)
+    inband = ref_data.GetRasterBand(1)
+    #nodata = inband.GetNoDataValue()
+    dzDEMArray = inband.ReadAsArray()
+    dzDEMArrayout = dzDEMArray[::-1]         # 8.12.15 gdal and netcdf4 have arrays in opposite directions
+    #dType = inband.DataType
+    ref_data = None
+
+    #Open file to write re-gridded data
+    ncOut = netCDF4.Dataset(output_netcdf,"r+") #, format='NETCDF4')
+    xout = ncOut.variables['x'][:]
+    yout = ncOut.variables['y'][:]
+    vardataType = ncOut.variables[varNameP].datatype
+    varin = numpy.zeros((len(yout),len(xout)),dtype=vardataType)
+    varout = numpy.zeros((len(yout),len(xout)),dtype=vardataType)
+    timeLen = len(ncOut.dimensions['time'])
+    tArray = ncOut.variables[time_var_name][:]
+    yLen = len(yout)
+    xLen = len(xout)
+
+    ncInT = netCDF4.Dataset(input_netcdfT,"r") # format='NETCDF4')
+    for tk in range(timeLen):
+        varin[:,:] = ncOut.variables[varNameP][tk,:,:]
+        varinT = ncInT.variables[varNameT][tk,:,:]
+        if timeUnits == 'days':
+            dateTimetk = datetime.strptime(baseDateTime,"%Y/%m/%d %H") + timedelta(days=int(tArray[tk]))
+        elif timeUnits == 'hours':
+            dateTimetk = datetime.strptime(baseDateTime,"%Y/%m/%d %H") + timedelta(hours=int(tArray[tk]))
+        else:
+            logger.info("Erroneous time unit")
+            retDictionary = {'success': "False", 'message': "Erroneous time unit"}
+            #print()
+            return retDictionary
+        monthtk = int(dateTimetk.month - 1)
+        #for yi in range(yLen):
+        #    for xj in range(xLen):
+        #       varout[yi,xj] = varin[yi,xj] - (dzDEMArrayout[yi,xj] * lapserateT[monthtk])  # bilinear_interpolation(yout[yi],xout[xj],points)
+        varout = varin* math.pow((((varinT+273.16) + dzDEMArrayout*-1*lapserateT[monthtk])/(varinT+273.16)),(-1*grav/(Rgas*-1*lapserateT[monthtk])))     #T in oC and lapserrate +ve
+        ncOut.variables[varNameP][tk,:,:] = varout[:,:]
+
+    ncOut.close()
+
+    logger.info("Success elevation adjustment Surface pressure")
+    retDictionary = {'success': "True", 'message': " Done surface pressure elevation adjustment"}
+    return retDictionary
 
 
